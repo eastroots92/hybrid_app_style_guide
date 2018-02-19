5f546ed825ad0f6f4b4b4de4ac5891b3a2bcf862
Eastroots92DESKTOP <eastroots92@gmail.com>
2018-02-19 10:46:46 +0900

    Header Scroll 이벤트 성능 최적화

--- javascripts/style.js
+++ javascripts/style.js
@@ -1,9 +1,22 @@
 /* Container 스크롤 시 Header 영역에 그림자 만들기 */
+    var isScroll;
+
     $(".container").scroll(function(){
+        isScroll = true;
+    });
+
+    setInterval(function(){
+        if (isScroll){
+            reactScroll();
+            isScroll = false;
+        }
+    }, 250);
+
+    function reactScroll(){
         var scroll_point = $('.container').scrollTop();
         if(scroll_point != 0){
             $('header.shadow').addClass("input_shadow");
         }else{
             $('header.shadow').removeClass("input_shadow");
         }
-    });
+    }
